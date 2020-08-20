995b60fc2be6198dfa1b203430967eba3205b05d
Kevin Giordani <giordani.kevin.02@gmail.com>
2020-08-20 12:36:09 +0200

    export interfacesé

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import { convertEntityToOpenApiFormat } from "./Entity/converters";
 import { convertRouteToOpenAPIJsonFormat } from "./Route/converters";
+export {
+  DeleteRoute,
+  GetRoute,
+  PostRoute,
+  PutRoute,
+  Route,
+} from "./Route/interfaces";
 
 export const convertRouteToOpenApi = (doc: any) => {
   return doc.simplified ? convertRouteToOpenAPIJsonFormat(doc) : doc;
